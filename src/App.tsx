f8d658dcd818c32c7611f6d8eeddd3db190fb46d
sashiksu <sashiksu@outlook.com>
2022-10-03 00:16:35 +0530

    check sample component

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { MyCounter } from "./index2";
+
+const App = () => {
+  return (
+    <>
+      <MyCounter value={2} />
+    </>
+  );
+};
+
+export default App;
