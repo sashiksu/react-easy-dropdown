119f2518e7d5173e713623382b704ced6b35f144
sashiksu <sashiksu@outlook.com>
2022-10-03 13:15:36 +0530

    Updates Live Demo index.html file

--- dev/index.js
+++ dev/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.tsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+/* 
+Do not remove strict mode, 
+so we can check where component tree throws warnings when we updates core packages
+*/
 root.render(
   <React.StrictMode>
     <App />
