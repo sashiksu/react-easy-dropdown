2d7c6b077e78a31b434cc934fe0de82f3601f983
sashiksu <sashiksu@outlook.com>
2022-10-03 14:00:28 +0530

    Added workaround for hot reloading issue in development mode

---
+++ dev/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { MyCounter } from "../src";
+
+const App = () => {
+  return (
+    <>
+      <h2>Inside dev</h2>
+      <MyCounter value={23} />
+    </>
+  );
+};
+
+export default App;
